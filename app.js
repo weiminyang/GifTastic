cc9fd634ed5e3ddae5461d4f1dea16d510af87a4
weimin yang <yangwm1983@gmail.com>
2019-06-12 06:36:36 -0700

    buttons add accomplish

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function displayInfo() {
   });
 
   // Adding a click event listener to all elements with a class of "topic-btn"
-  $(document).on("click", ".topic-btn", displaytopicInfo);
+  $(document).on("click", ".topic-btn", displayInfo);
 
   // Calling the renderButtons function to display the intial buttons
   renderButtons();
